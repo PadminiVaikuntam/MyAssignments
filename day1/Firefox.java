c62dfc98492724d1358ad09d3d793d7b0d100a0b
PadminiVaikuntam <pvaikuntam@gmail.com>
2024-06-23 15:04:06 +0530

    Add files via upload
    day 1 commit changes

---
+++ day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	float browserVersion=1002.f;
+	String browserName="firefox";
+	boolean isVisible = true;
+	int year=1998;
+	char browserLogo='f';
+	System.out.println("browserVersion "+browserVersion);
+	System.out.println("browserName "+browserName);
+	System.out.println("isVisible "+isVisible);
+	System.out.println("releaseYear "+year);
+	System.out.println("browserlogo "+browserLogo);
+	}
+}
